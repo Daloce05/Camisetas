5b55f8196daefd981995efdee853aaa3f4d67b31
David <david@example.com>
2026-05-06 22:13:04 -0500

    front movil correccion

--- Front/src/components/navbar/AuthButtons.jsx
+++ Front/src/components/navbar/AuthButtons.jsx
@@ -1,9 +1,9 @@
 const AuthButtons = () => {
   return (
-    <div className="py-4  flex justify-center items-center gap-4 flex-wrap">
-      <button className="btn btn-neutral btn-outline"> crear cuenta</button>
+    <div className="py-2 flex flex-col lg:flex-row justify-center items-center gap-2 lg:gap-4 w-full">
+      <button className="btn btn-neutral btn-outline w-full lg:w-auto">crear cuenta</button>
       <div className="hidden lg:block">|</div>
-      <button className="btn bg-neutral btn-outline"> iniciar sesion</button>
+      <button className="btn bg-neutral btn-outline w-full lg:w-auto">iniciar sesión</button>
     </div>
   )
 }
